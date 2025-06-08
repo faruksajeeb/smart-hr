0ba3822ef96d0a17d60569d13a8418adf42f94a0
faruksajeeb <ofsajeeb@gmail.com>
2025-06-08 20:24:03 +0600

    laravel react default auth and post crud

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $posts = Post::all();
+        return Inertia::render('posts/index',['posts'=>$posts]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return Inertia::render('posts/create',[]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required',
+            'body' => 'required'
+        ]);
+
+        Post::create([
+            'title' => $request->title,
+            'body' => $request->body,
+        ]);
+
+        return redirect()->route('posts.index');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        $post = Post::find($id);
+        return Inertia::render('posts/edit',['post'=>$post]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $request->validate([
+            'title' => 'required',
+            'body' => 'required'
+        ]);
+
+        $post = Post::find($id);
+        $post->title = $request->title;
+        $post->body = $request->body;
+        $post->save();
+
+        return redirect()->route('posts.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        Post::destroy($id);
+        return redirect()->route('posts.index');
+    }
+}
---
+++ resources/js/pages/posts/index.tsx
@@ -0,0 +1,77 @@
+import { PlaceholderPattern } from '@/components/ui/placeholder-pattern';
+import AppLayout from '@/layouts/app-layout';
+import { type BreadcrumbItem } from '@/types';
+import { Head, Link ,usePage, useForm } from '@inertiajs/react';
+import { FormEventHandler } from 'react';
+
+const breadcrumbs: BreadcrumbItem[] = [
+    {
+        title: 'Posts',
+        href: '/posts',
+    },
+];
+
+export default function Posts() {
+    const {posts} = usePage().props
+    const {delete: destroy} = useForm();
+    const destroyPost: FormEventHandler = (e, id) => {
+        e.preventDefault();
+        if(confirm('Are you sure to delete this?')){
+            destroy(route('posts.destroy',id));
+        }
+    }
+    return (
+        <AppLayout breadcrumbs={breadcrumbs}>
+            <Head title="Posts" />
+            <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4 overflow-x-auto">
+                <div>
+                <Link 
+                    href={route('posts.create')}
+                    className='px-3 py-2 bg-blue-900 text-white rounded-lg '
+                >
+                Create Post
+                </Link>
+
+                </div>
+                <table className="w-full text-sm ">
+                    <thead className="text-xs bg-gray-50 text-gray-700">
+                        <tr>
+                            <th scope='col' className='py-3'>SL NO</th>
+                            <th scope='col' className='py-3'>Title</th>
+                            <th scope='col' className='py-3'>Body</th>
+                            <th scope='col' className='py-3'>Publication Status</th>
+                            <th scope='col' className='py-3'>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            posts.map(({id,title,body})=>(
+
+                        <tr className='odd:bg-white even:bg-gray-50'>
+                            <td className='px-6 py-2 '>{id}</td>
+                            <td className='px-6 py-2 '>{title}</td>
+                            <td className='px-6 py-2 '>{body}</td>
+                            <td className='px-6 py-2 '></td>
+                            <td className='px-6 py-2 text-center'>
+
+                            <form onSubmit={(e)=>destroyPost(e,id)}>
+                                <Link 
+                                href={route('posts.edit',id)} 
+                                className='px-3 py-2 text-xs font-medium bg-amber-600 text-white rounded-lg mx-1'>
+                                    Edit
+                                </Link>
+                                <button className='px-3 py-2 text-xs font-medium bg-red-700 text-white rounded-lg mx-1'>
+                                    Delete
+                                </button>
+                                </form>
+                            </td>
+                        </tr>
+
+))
+}
+                    </tbody>
+                </table>
+            </div>
+        </AppLayout>
+    );
+}
